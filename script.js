49009a72bccbd2755b0679d90246c81723f57d44
moh3384 <smhnd657@gmail.com>
2026-06-14 18:01:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const projects = [
     fullDesc: "منصة تجارة إلكترونية شاملة تتيح عرض المنتجات، إدارة سلة التسوق، وتجربة تسوق عصرية. تتميز بواجهة مستخدم جذابة وأداء عالي.",
     link: "https://moh3384.github.io/test/",
     icon: "🛒",
-    gallery: ["assets/images/img45.png", "assets/images/img46.png", "assets/images/img47.png", "assets/images/img48.png"],
+    gallery: ["assets/images/img45.jpeg", "assets/images/img46.jpeg", "assets/images/img47.jpeg", "assets/images/img48.jpeg"],
     tags: ["HTML5", "CSS3", "JavaScript", "PHP","MYSQL"],
   },
 
